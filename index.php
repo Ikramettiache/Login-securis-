3b249e9130428f594db1523d414f64cc0c9bb443
Ikramettiache <ikramettiache12@gmail.com>
2021-11-01 18:58:33 +0100

    Modify 2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>TEST </title>
+	<title>Page d'acceuil </title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
@@ -18,7 +18,7 @@
 	 <div class="d-flex justify-content-center align-items-center flex-column" style="min-height: 100vh;">
 	 	
         <h1 class="text-center display-4" style="margin-top: -60px;font-size: 2rem">Hi</h1>
-        <a href="private\logout.php" class="btn btn-warning">LOGOUT</a>
+        <a href="private\logout.php" class="btn btn-warning">Se déconnecter</a>
 
 	 </div>
 </body>

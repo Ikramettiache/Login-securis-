6b1e241cb299a41ccb31764eb9dddfd1089964ea
Ikramettiache <ikramettiache12@gmail.com>
2021-11-01 16:49:39 +0100

    ADD index File

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+  session_start();
+
+  if (isset($_SESSION['user_id']) && isset($_SESSION['user_email'])) { 
+  	
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>INDEX PAGE</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+</head>
+<body>
+	 <div class="d-flex justify-content-center align-items-center flex-column" style="min-height: 100vh;">
+	 	
+        <h1 class="text-center display-4" style="margin-top: -60px;font-size: 2rem">Hi</h1>
+        <a href="private\logout.php" class="btn btn-warning">LOGOUT</a>
+
+	 </div>
+</body>
+</html>
+<?php 
+}else {
+   header("Location: private\login.php");
+}
+ ?>
--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['user_email'])) {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Secure login System PHP</title>
+	<title>Secure login</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
